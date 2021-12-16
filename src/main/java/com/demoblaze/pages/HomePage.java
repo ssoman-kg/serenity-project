24add7defa033de61e3b23abc2e5072553d012a7
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2021-12-16 18:52:51 -0500

    Home page modified

--- src/main/java/com/demoblaze/pages/HomePage.java
+++ src/main/java/com/demoblaze/pages/HomePage.java
@@ -2,49 +2,6 @@ package com.demoblaze.pages;
 
 public class HomePage  extends BasePage{
 
-    private static final String SIGN_UP_LINK = "//a[contains(text(),'Sign up')]";
-    private static final String USER_NAME_FIELD = "//input[@id='sign-username']";
-    private static final String PASSWORD_FIELD = "//input[@id='sign-password']";
-    private static final String SIGN_UP_BUTTON = "//button[contains(text(),'Sign up')]";
 
-
-    /**
-     * Add New User Information
-     *
-     * @param userName
-     * @param password
-     * @return
-     */
-    public HomePage openStore() {
-        open();
-        waitForTextToAppear("PRODUCT STORE"); //use explicit wait for the text to appear
-        return this;
-
-    }
-
-    public HomePage navigateToSignUpLink() {
-
-        waitFor(SIGN_UP_LINK).$(SIGN_UP_LINK).click();
-        return this;
-
-    }
-    public HomePage addNewUserInfo(String userName , String password) {
-
-        $(USER_NAME_FIELD).type(userName);
-        $(PASSWORD_FIELD).type(password);
-
-        return this;
-    }
-    /**
-     * Clicking Signup button
-     *
-     * @return
-     */
-    public HomePage clickSignUp() {
-
-        waitFor(SIGN_UP_BUTTON).$(SIGN_UP_BUTTON).click();
-
-        return this;
-    }
 
 }
