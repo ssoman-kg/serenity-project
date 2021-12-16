f6b3c59ab015bd05218a6684ce340a15ecd313b7
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2021-12-16 18:56:24 -0500

    SignUp steps and runner created

---
+++ src/test/java/com/demoblaze/cucumber/steps/SignupSteps.java
@@ -0,0 +1,47 @@
+package com.demoblaze.cucumber.steps;
+
+import com.demoblaze.cucumber.steps.serenity.DemoblazeSteps;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class SignupSteps {
+
+    @Steps
+    DemoblazeSteps user;
+
+    @Managed
+    WebDriver driver;
+
+    @Given("I navigate to the Home Page")
+    public void i_navigate_to_the_home_page() {
+        user.navigateToHomePage();
+    }
+    @And("I click on the Sign up link")
+    public void i_click_on_the_signup_link() {
+        user.navigateToSignUpPage();
+    }
+    @And("I add user name and password")
+    public void i_add_userName_and_password() {
+        String userName = "user001";
+        String password = "test123";
+        user.addNewUserInfo(userName, password);
+    }
+    @And("I click on sign up")
+    public void i_click_on_sign_up() {
+        user.signUp();
+    }
+    @Then("I must be able to view the alert message saying the user exist")
+    public void iMustBeAbleToViewTheAlertMessageSayingTheUserExist() {
+        Alert alert = driver.switchTo().alert();
+        String alertText=alert.getText();
+        assertEquals("This user already exist.", alertText);
+    }
+}
+
