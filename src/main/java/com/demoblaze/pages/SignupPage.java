a77a2e69dd59f8bfa947a173d8e03beab063d663
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2021-12-16 18:53:27 -0500

    sign up page added

---
+++ src/main/java/com/demoblaze/pages/SignupPage.java
@@ -0,0 +1,25 @@
+package com.demoblaze.pages;
+
+public class SignupPage extends BasePage{
+
+
+    private static final String USER_NAME_FIELD = "//input[@id='sign-username']";
+    private static final String PASSWORD_FIELD = "//input[@id='sign-password']";
+    private static final String SIGN_UP_BUTTON = "//button[contains(text(),'Sign up')]";
+
+    public SignupPage addNewUserInfo(String userName , String password) {
+        waitForTextToAppear("Username:");
+        waitForTextToAppear("Password:");
+        waitFor(USER_NAME_FIELD).$(USER_NAME_FIELD).type(userName);
+        waitFor(PASSWORD_FIELD).$(PASSWORD_FIELD).type(password);
+        return null;
+    }
+        public HomePage signUp() {
+        waitForTextToAppear("Sign up");
+        waitFor(SIGN_UP_BUTTON).$(SIGN_UP_BUTTON).click();
+        return null;
+
+    }
+
+
+}
