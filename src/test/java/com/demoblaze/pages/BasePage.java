8cd45dec19e99b9caccf51331f62aa52ce88baa6
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2022-01-06 14:40:08 -0500

    Method body changed and added overloading

--- src/test/java/com/demoblaze/pages/BasePage.java
+++ src/test/java/com/demoblaze/pages/BasePage.java
@@ -4,14 +4,17 @@ import net.serenitybdd.core.pages.PageObject;
 
 public class BasePage extends PageObject {
 
-    public void enterField(String fieldText, String Field, String value) {
-        waitForTextToAppear(fieldText);
-        waitFor(Field).$(Field).type(value);
+    public void inputValue(String field, String value) {
+        waitFor(field).$(field).type(value);
     }
 
-    public void click(String text, String button) {
+    public void click(String locator) {
+        waitFor(locator).$(locator).click();
+    }
+    // Overloaded Method
+    public void click(String text, String locator) {
         waitForTextToAppear(text);
-        waitFor(button).$(button).click();
+        waitFor(locator).$(locator).click();
     }
 }
 
--- src/test/java/com/demoblaze/pages/SignupPage.java
+++ src/test/java/com/demoblaze/pages/SignupPage.java
@@ -8,11 +8,11 @@ public class SignupPage extends BasePage{
     private static final String SIGN_UP_BUTTON = "//button[contains(text(),'Sign up')]";
 
     public void addNewUserInfo(String userName , String password) {
-        enterField("Username:", USER_NAME_FIELD, userName);
-        enterField("Password:", PASSWORD_FIELD, password);
+        inputValue(USER_NAME_FIELD, userName);
+        inputValue(PASSWORD_FIELD, password);
     }
     public void signUp() {
-        click("Sign up", SIGN_UP_BUTTON);
+        click(SIGN_UP_BUTTON);
     }
 
 
