65548e5e8c29e459fe8f2621474eba83f0fb2a10
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2022-02-02 12:35:03 -0500

    corresponding step file updated

---
+++ src/test/java/com/demoblaze/cucumber/steps/NavigationSteps.java
@@ -0,0 +1,80 @@
+package com.demoblaze.cucumber.steps;
+
+import com.demoblaze.pages.BasePage;
+import com.demoblaze.pages.HomePage;
+import com.demoblaze.pages.LoginPage;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+
+
+public class NavigationSteps extends ScenarioSteps {
+
+    LoginPage loginPage;
+    HomePage homePage;
+
+    /***********************************
+     *
+     * BASE STEPS
+     /***********************************
+     */
+
+    @Step("Navigating to login Link")
+    public void navigateToLogin() {
+        homePage.navigateTologin();
+    }
+
+    @Step("Entering user info")
+    public void enterUserInfo(String userName, String password) {
+        loginPage.enterUserInfo(userName, password);
+    }
+
+    @Step("Clicking on Log in button")
+    public void login() {
+        loginPage.login();
+    }
+
+    @Step("Clicking on Welcome page ")
+    public void navigateToWelcomePage() {
+
+        homePage.navigateToWelcomePage();
+    }
+
+    @Step("Read Welcome text ")
+    public String getWelcomeText() {
+        return homePage.getWelcomeText();
+    }
+
+    @Step("Navigating to Home page")
+    public void navigateToHomePage() {
+
+        homePage.navigateToHomePage();
+    }
+
+    public void navigateToHomeLink() {
+        homePage.navigateToHomeLink();
+    }
+
+    public void navigateToContact() {
+        homePage.navigateToContact();
+    }
+
+    public void navigateToAboutUs() {
+        homePage.navigateToAboutUs();
+    }
+
+    public void navigateToCart() {
+        homePage.navigateToCart();
+    }
+
+    public void navigateTologin() {
+        homePage.navigateTologin();
+    }
+
+    public void navigateToLogOut() {
+        homePage.navigateToLogOut();
+    }
+
+    public void navigateToSignUp() {
+        homePage.navigateToSignUp();
+    }
+}
