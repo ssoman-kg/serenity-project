3c54ea19b0b613a5dd33e9121390d829c126e396
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2022-02-12 00:41:16 -0500

    About and Contact page aded

---
+++ src/test/java/com/demoblaze/pages/ContactPage.java
@@ -0,0 +1,26 @@
+package com.demoblaze.pages;
+
+public class ContactPage extends BasePage {
+
+
+    private static final String CONTACT_MODAL_TITLE = "//h5[contains(@class, 'modal-title') and text() = 'New message']";
+    private static final String CONTACT_EMAIL = "//*[@id='recipient-email']";
+    private static final String CONTACT_NAME = "//*[@id='recipient-name']";
+    private static final String CONTACT_MESSSAGE = "//*[@id='message-text']";
+    private static final String SEND_BUTTON = "//button[contains(text(),'Send message')]";
+
+
+    public String findTitle() {
+        return getText(CONTACT_MODAL_TITLE);
+    }
+
+    public void enterContactMessage(String email, String name, String message) {
+        inputValue(CONTACT_EMAIL, email);
+        inputValue(CONTACT_NAME, name);
+        inputValue(CONTACT_MESSSAGE, message);
+    }
+
+    public void sendMessage() {
+        click(SEND_BUTTON);
+    }
+}
