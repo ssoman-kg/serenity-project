8d404d2e50c1a462c435f35b5fed150120fd0a2e
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2022-02-27 15:58:20 -0500

    steps modified

--- src/test/java/com/demoblaze/pages/ContactPage.java
+++ src/test/java/com/demoblaze/pages/ContactPage.java
@@ -6,9 +6,8 @@ public class ContactPage extends BasePage {
     private static final String CONTACT_MODAL_TITLE = "//h5[contains(@class, 'modal-title') and text() = 'New message']";
     private static final String CONTACT_EMAIL = "//*[@id='recipient-email']";
     private static final String CONTACT_NAME = "//*[@id='recipient-name']";
-    private static final String CONTACT_MESSSAGE = "//*[@id='message-text']";
+    private static final String CONTACT_MESSAGE = "//*[@id='message-text']";
     private static final String SEND_BUTTON = "//button[contains(text(),'Send message')]";
-
 
     public String findTitle() {
         return getText(CONTACT_MODAL_TITLE);
@@ -17,11 +16,10 @@ public class ContactPage extends BasePage {
     public void enterContactMessage(String email, String name, String message) {
         inputValue(CONTACT_EMAIL, email);
         inputValue(CONTACT_NAME, name);
-        inputValue(CONTACT_MESSSAGE, message);
+        inputValue(CONTACT_MESSAGE, message);
     }
 
     public void clickSendMessage() {
         click(SEND_BUTTON);
     }
-
 }
