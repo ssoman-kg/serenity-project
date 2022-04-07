3ae60033e64123a915b4ab4cbf3cfebde5894bd2
Suparna Soman <suparnasoman@MBPl19C02D1JP1MD6M.local>
2022-04-07 11:49:56 -0400

    Feature steps defined and added

--- src/test/java/com/demoblaze/cucumber/featureSteps/AddToCartFeatureSteps.java
+++ src/test/java/com/demoblaze/cucumber/featureSteps/AddToCartFeatureSteps.java
@@ -17,4 +17,57 @@ public class AddToCartFeatureSteps {
     }
 
 
+    @Given("user is on a Product page")
+    public void userIsOnAProductPage() {
+        
+    }
+
+    @When("user clicks on Add to cart")
+    public void userClicksOnAddToCart() {
+        
+    }
+
+    @Then("Product should show on cart page")
+    public void productShouldShowOnCartPage() {
+        
+    }
+
+    @When("user returns to home page")
+    public void userReturnsToHomePage() {
+        
+    }
+
+    @Then("other products are avaialble on the home page")
+    public void otherProductsAreAvaialbleOnTheHomePage() {
+        
+    }
+
+    @When("user clicks on a product")
+    public void userClicksOnAProduct() {
+        
+    }
+
+    @Then("the product page should show")
+    public void theProductPageShouldShow() {
+        
+    }
+
+    @When("user add the product to cart")
+    public void userAddTheProductToCart() {
+        
+    }
+
+    @Then("the new product should be available on the cart page")
+    public void theNewProductShouldBeAvailableOnTheCartPage() {
+        
+    }
+
+    @When("user deletes a product from cart page")
+    public void userDeletesAProductFromCartPage() {
+        
+    }
+
+    @Then("the product should not show on cart page")
+    public void theProductShouldNotShowOnCartPage() {
+    }
 }
--- src/test/resources/features/add-to-cart.feature
+++ src/test/resources/features/add-to-cart.feature
@@ -1,7 +1,15 @@
-Feature: User plays video in the About us modal
+Feature: User adds product to the cart
 
   @test1
   Scenario: Play video on About us modal
-    Given user is at the DemoBlaze Landing page
-    When user clicks on About us link
-    Then About us modal should open
+    Given user is on a Product page
+    When user clicks on Add to cart
+    Then Product should show on cart page
+    When user returns to home page
+    Then other products are avaialble on the home page
+    When user clicks on a product
+    Then the product page should show
+    When user add the product to cart
+    Then the new product should be available on the cart page
+    When user deletes a product from cart page
+    Then the product should not show on cart page
